1972df5a0936a6748948b224e4032b6ca1852128
Po Chun, Lu <oceanus11034@gmail.com>
2018-11-03 00:16:18 +0800

    add binary tree generator

--- data_generator.py
+++ data_generator.py
@@ -10,10 +10,22 @@ class Tree:
         else:
             self.children[value] = Tree(feature)
 
+        return self.children[value]
 
 
+def generate_binary_tree(level):
+    def grow(current_node, current_level, final_level):
+        if final_level != current_level:
+            left_child = current_node.insert_node('left', current_level)
+            right_child = current_node.insert_node('right', current_level)
+            grow(left_child, current_level+1, final_level)
+            grow(right_child, current_level+1, final_level)
+
+    root = Tree(1)
+    grow(root, 1, level)
+    return root
 
 
 # TODO: class RuleGenerator
 
-# TODO: class dataGenerator
+# TODO: class DataGenerator
