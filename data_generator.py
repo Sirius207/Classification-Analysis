dfa9ae2d527a20f394515bc94e935be46737e55a
Po Chun, Lu <oceanus11034@gmail.com>
2018-11-16 22:53:32 +0800

    add process generate data

--- data_generator.py
+++ data_generator.py
@@ -5,20 +5,20 @@ class Tree:
         self.feature = feature
         self.children = None
 
-    def insert_node(self, value, feature):
+    def insert_node(self, feature):
+        new_node = Tree(feature)
         if self.children == None:
-            self.children = {}
-            self.children[value] = Tree(feature)
+            self.children = []
+            self.children.append(new_node)
         else:
-            self.children[value] = Tree(feature)
+            self.children.append(new_node)
 
-        return self.children[value]
+        return self.children[-1]
 
 
 def generate_random_tree(level, max_degree, is_full_degree=False):
     def grow(current_node, current_level, final_level, max_degree):
         if final_level != current_level:
-
 
             if is_full_degree:
                 current_node_degree = max_degree
@@ -29,7 +29,7 @@ def generate_random_tree(level, max_degree, is_full_degree=False):
                 current_node.feature = 'A' if random.randint(0, 1) == 0 else 'B'
             else:
                 for number in range(current_node_degree):
-                    new_child = current_node.insert_node('C'+str(number), 'L'+str(current_level))
+                    new_child = current_node.insert_node('L'+str(current_level))
                     grow(new_child, current_level+1, final_level, max_degree)
         else:
             current_node.feature = 'A' if random.randint(0, 1) == 0 else 'B'
@@ -39,26 +39,86 @@ def generate_random_tree(level, max_degree, is_full_degree=False):
     return root
 
 
-def tree_to_dict(tree):
-    def traversal(node, tree_dict):
+def tree_to_list(tree):
+    def traversal(node, tree_list):
         if node.children != None:
-            tree_dict[node.feature] = {}
-            for value in node.children:
-                if node.children[value].children != None:
-                    tree_dict[node.feature][value] = {}
-                    sub_dict = tree_dict[node.feature][value]
-                    sub_node = node.children[value]
+            for id, value in enumerate(node.children):
+                if node.children[id].children != None:
+                    tree_list.append([])
+                    sub_dict = tree_list[-1]
+                    sub_node = node.children[id]
                     traversal(sub_node, sub_dict)
                 else:
-                    tree_dict[node.feature][value] = node.children[value].feature
+                    tree_list.append(node.children[id].feature)
         else:
-            tree_dict = node.feature
+            tree_list = node.feature
 
-    tree_dict = {}
-    traversal(tree, tree_dict)
-    return tree_dict
-
-
+    tree_list = []
+    traversal(tree, tree_list)
+    return tree_list
 
 
-# TODO: class DataGenerator
+#
+# Part 2: Data Generation
+#
+
+
+def get_label(data, rules):
+    features = data.split(',')
+    sub_choice = rules
+    for feature in features:
+        sub_choice = sub_choice[int(feature)]
+
+    return sub_choice
+
+def get_random_data(options, rules):
+    data_list = []
+    for data_id in range(options['data_length']):
+        new_data = ''
+        # create feature values
+        for column_id in range(options['x_columns']):
+            new_value = str(random.randint(0, options['choices']-1))
+            if (len(new_data) == 0):
+                new_data += new_value
+            else:
+                new_data +=  ',' + new_value
+
+        # add y value
+        if (rules):
+            label = get_label(new_data, rules)
+            new_data +=  ',' + label
+
+        data_list.append(new_data)
+    return data_list
+
+def write_data_file(data_list):
+    head = ''
+    for id, num in enumerate(data_list[0].split(',')):
+        new_value = 'F' + str(id)
+        if (len(head) == 0):
+            head += new_value
+        else:
+            head += ',' + new_value
+
+    with open('output.csv', 'w') as output:
+        output.write(head + '\n')
+        for data in data_list:
+            output.write(data + '\n')
+
+#
+# Part 3: Main
+#
+
+options = {
+    'x_columns': 2,
+    'choices': 2,
+    'data_length': 5
+}
+
+root = generate_random_tree(options['x_columns'] + 1, options['choices'], True)
+rules = tree_to_list(root)
+
+print(rules)
+
+data = get_random_data(options, rules)
+write_data_file(data)
