e7023db9799d0ce48e3a5fd87b6ae1b4ae195c59
Po Chun, Lu <oceanus11034@gmail.com>
2018-11-17 22:57:37 +0800

    add more data config

--- decision_tree.py
+++ decision_tree.py
@@ -12,9 +12,15 @@ logger.setLevel(level=logging.INFO)
 data_config = [
     'r3_f3_c5',
     'r3_f6_c5',
-    'r3_f9_c5'
+    'r3_f9_c5',
+    'r3_f12_c5',
+    'r3_f15_c5'
 ]
 training_propotion_list = [0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+testing_propotion = 0.1
+
+fit_index = '50'
+basic_path = 'data/{}/'.format(fit_index)
 
 
 def calculate_acc(data, training_propotion):
@@ -30,10 +36,11 @@ def calculate_acc(data, training_propotion):
     clf = clf.fit(training_x, training_y)
 
     # testing
-    testing_x = data.values[training_data_size:,0:x_column_size]
-    predict_y = clf.predict(testing_x)
-    test_y = data['y'][training_data_size:]
+    testing_data_size = int((1 - testing_propotion) * len(data))
+    testing_x = data.values[testing_data_size:,0:x_column_size]
+    test_y = data['y'][testing_data_size:]
 
+    predict_y = clf.predict(testing_x)
     acc = accuracy_score(test_y,predict_y)*100
 
     logger.info(f'Training Percent: {training_propotion}, Acc: {acc}')
@@ -51,7 +58,7 @@ def iterate_each_training_data(data_config):
     transaction_list = []
     for data_path in data_config:
         logger.info(f'Training Data: {data_path} ----------------')
-        data = pd.read_csv('data/' + data_path + '/training.csv')
+        data = pd.read_csv(basic_path + data_path + '/training.csv')
         acc_list = iterate_each_training_propotion(data)
         transcation = data_path + ',' + ','.join(str(acc) for acc in acc_list)
         transaction_list.append(transcation)
@@ -59,7 +66,7 @@ def iterate_each_training_data(data_config):
 
 
 def write_acc_results(transaction_list):
-    with open('data/results.csv', 'w') as output:
+    with open(basic_path + 'results.csv', 'w') as output:
         head = 'type,' + ','.join(str(p) for p in training_propotion_list) + '\n'
         output.write(head)
 
