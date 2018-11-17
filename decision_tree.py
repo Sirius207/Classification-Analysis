f509b86fd28405dfcc5096ad6ff619e50536d046
Po Chun, Lu <oceanus11034@gmail.com>
2018-11-17 14:59:56 +0800

    add auto calculate acc

--- decision_tree.py
+++ decision_tree.py
@@ -1,26 +1,44 @@
+import logging
 import pandas as pd
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 
-training_data_propotion = 0.8
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
 
-# get data
-data = pd.read_csv('training.csv')
-training_data_size = int(training_data_propotion * len(data))
-x_column_size = len(data.iloc[0]) - 1
 
-training_x = data.values[0:training_data_size,0:x_column_size]
-training_y = data['y'][0:training_data_size]
+# Constants
+training_propotion_list = [0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
 
-# training
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(training_x, training_y)
+data = pd.read_csv('data/training.csv')
 
-# testing
-testing_x = data.values[training_data_size:,0:x_column_size]
-predict_y = clf.predict(testing_x)
-test_y = data['y'][training_data_size:]
 
-acc = accuracy_score(test_y,predict_y)*100
+def calculate_acc(data, training_propotion):
+    # split training data
+    training_data_size = int(training_propotion * len(data))
+    x_column_size = len(data.iloc[0]) - 1
 
-print(acc)
+    training_x = data.values[0:training_data_size,0:x_column_size]
+    training_y = data['y'][0:training_data_size]
+
+    # training
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(training_x, training_y)
+
+    # testing
+    testing_x = data.values[training_data_size:,0:x_column_size]
+    predict_y = clf.predict(testing_x)
+    test_y = data['y'][training_data_size:]
+
+    acc = accuracy_score(test_y,predict_y)*100
+
+    logger.info(f'Training Percent: {training_propotion}, Acc: {acc}')
+    return acc
+
+
+def iterate_each_training_propotion(data):
+    for propotion in training_propotion_list:
+        calculate_acc(data, propotion)
+
+iterate_each_training_propotion(data)
