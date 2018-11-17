0428b6f3611e565e63556e652d60d3168940ce39
Po Chun, Lu <oceanus11034@gmail.com>
2018-11-18 00:12:13 +0800

    add save depth & node count function

--- decision_tree.py
+++ decision_tree.py
@@ -19,7 +19,7 @@ data_config = [
 training_propotion_list = [0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
 testing_propotion = 0.1
 
-fit_index = '50'
+fit_index = '100'
 basic_path = 'data/{}/'.format(fit_index)
 
 
@@ -42,37 +42,72 @@ def calculate_acc(data, training_propotion):
 
     predict_y = clf.predict(testing_x)
     acc = accuracy_score(test_y,predict_y)*100
-
     logger.info(f'Training Percent: {training_propotion}, Acc: {acc}')
-    return acc
+
+    # graph
+    max_depth = clf.tree_.__getstate__()['max_depth']
+    node_count = clf.tree_.__getstate__()['node_count']
+    logger.info(f'Max depth: {max_depth}')
+    logger.info(f'Node count: {node_count}')
+
+    return (acc, max_depth, node_count)
 
 
 def iterate_each_training_propotion(data):
     acc_list = []
+    depth_list = []
+    node_count_list = []
     for propotion in training_propotion_list:
-        acc_list.append(calculate_acc(data, propotion))
-    return acc_list
+        results = calculate_acc(data, propotion)
+        acc_list.append(results[0])
+        depth_list.append(results[1])
+        node_count_list.append(results[2])
+    return (acc_list, depth_list, node_count_list)
 
 
 def iterate_each_training_data(data_config):
-    transaction_list = []
+    def generate_transaction_list(data_path, single_results_list):
+        return data_path + ',' + ','.join(str(acc) for acc in single_results_list)
+
+    acc_transaction_list = []
+    depth_transaction_list = []
+    node_transaction_list = []
+
     for data_path in data_config:
         logger.info(f'Training Data: {data_path} ----------------')
         data = pd.read_csv(basic_path + data_path + '/training.csv')
-        acc_list = iterate_each_training_propotion(data)
-        transcation = data_path + ',' + ','.join(str(acc) for acc in acc_list)
-        transaction_list.append(transcation)
-    return transaction_list
+        results_list = iterate_each_training_propotion(data)
+
+        acc_transcation = generate_transaction_list(data_path, results_list[0])
+        acc_transaction_list.append(acc_transcation)
+
+        depth_transcation = generate_transaction_list(data_path, results_list[1])
+        depth_transaction_list.append(depth_transcation)
+
+        node_transcation = generate_transaction_list(data_path, results_list[2])
+        node_transaction_list.append(node_transcation)
 
 
-def write_acc_results(transaction_list):
-    with open(basic_path + 'results.csv', 'w') as output:
-        head = 'type,' + ','.join(str(p) for p in training_propotion_list) + '\n'
+    return (acc_transaction_list, depth_transaction_list, node_transaction_list)
+
+
+def write_results(transaction_list):
+    def write_data(output, head, transaction_list):
         output.write(head)
 
         for transaction in transaction_list:
             output.write(transaction + '\n')
 
+    with open(basic_path + 'acc.csv', 'w') as acc_output, \
+        open(basic_path + 'depth.csv', 'w') as depth_output, \
+        open(basic_path + 'node.csv', 'w') as node_output:
+
+        head = 'type,' + ','.join(str(p) for p in training_propotion_list) + '\n'
+
+        write_data(acc_output, head, transaction_list[0])
+        write_data(depth_output, head, transaction_list[1])
+        write_data(node_output, head, transaction_list[2])
+
 
 transaction_list = iterate_each_training_data(data_config)
-write_acc_results(transaction_list)
+write_results(transaction_list)
