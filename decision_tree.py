aeb491d36a0113f3682153f28150e214d0e4dbe5
Po Chun, Lu <oceanus11034@gmail.com>
2018-11-18 17:43:13 +0800

    update training propotion

--- decision_tree.py
+++ decision_tree.py
@@ -16,10 +16,10 @@ data_config = [
     'r3_f12_c5',
     'r3_f15_c5'
 ]
-training_propotion_list = [0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+training_propotion_list = [0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9]
 testing_propotion = 0.1
 
-fit_index = '100'
+fit_index = '50'
 basic_path = 'data/{}/'.format(fit_index)
 
 
